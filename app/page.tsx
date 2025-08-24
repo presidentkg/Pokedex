fa65c7e170daa467c60ee68b1d5a141bee67a647
majazocom <maja.gedda@zocom.se>
2025-08-24 22:07:54 +0200

    btn-primary example with tailwind apply

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main>
       <section className="flex flex-col items-center gap-4 bg-gradient-to-br [background-image:linear-gradient(-10deg,_#C97FE4,_#AECDF6)] p-14">
-        <h1 className="text-center text-8xl font-extrabold text-transparent bg-gradient-to-r from-purple-800 to-blue-800 [background-clip:text]">Gotta catch 'em all!</h1>
-        <p className="text-center text-white text-xl">Discover, search and explore the amazing world of Pokémon. Find<br/> your favourite and learn about their stats.</p>
-        <button className="p-1 pl-3 pr-3 mt-2 text-center cursor-pointer text-xl text-white bg-gradient-to-r from-orange-400 to-red-400 rounded-full font-bold">Random Pokémon</button>
+        <h1 className="text-center mt-14 text-8xl font-extrabold text-transparent bg-gradient-to-r from-purple-800 to-blue-800 [background-clip:text]">Gotta catch 'em all!</h1>
+        <p className="text-center text-white text-xl">Discover, search and explore the amazing world of Pokémon. Find<br /> your favourite and learn about their stats.</p>
+        <button className="btn-primary">
+          <Image
+            src="/Dice.svg"
+            width={25}
+            height={2250}
+            alt="Dice"
+          />
+          Random Pokémon</button>
       </section>
     </main>
   );
