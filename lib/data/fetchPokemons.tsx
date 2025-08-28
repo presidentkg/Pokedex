1aa2b1a87c0c7e64f2c26f8aa3087a0f7b8f89a6
Karl Ragnar <karlragnar.kling@gmail.com>
2025-08-28 17:58:47 +0200

    Simplyfied the code and added a sorting

--- lib/data/fetchPokemons.tsx
+++ lib/data/fetchPokemons.tsx
@@ -9,9 +9,8 @@ export async function fetchPokemon(
         const responses = await Promise.all(fetchPromises);
         if (responses.some((res) => !res.ok))
             throw new Error("One or more requests failed");
-        const jsonPromises = responses.map((res) => res.json());
-        const fetchedPokemons = await Promise.all(jsonPromises);
-        const transformedPokemons: PokemonData[] = fetchedPokemons.map((pokemon) => ({
+        const pokemonData = await Promise.all(responses.map((res) => res.json()));
+        const transformedPokemons: PokemonData[] = pokemonData.map((pokemon) => ({
             name: pokemon.name,
             id: pokemon.id,
             image: pokemon.sprites.other["official-artwork"].front_default || null,
@@ -20,6 +19,7 @@ export async function fetchPokemon(
             attack: pokemon.stats.find((statObj: PokemonStat) => statObj.stat.name === 'attack')?.base_stat || 0,
             defense: pokemon.stats.find((statObj: PokemonStat) => statObj.stat.name === 'defense')?.base_stat || 0,
         }));
+        transformedPokemons.sort((a, b) => a.id - b.id);
         return transformedPokemons;
     } catch (e) {
         return [];
