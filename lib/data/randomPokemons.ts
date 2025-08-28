2af6b8d5a0fa7091339084f62ec088e05aff3041
Karl Ragnar <karlragnar.kling@gmail.com>
2025-08-28 17:58:00 +0200

    Made the fetch recursive

--- lib/data/randomPokemons.ts
+++ lib/data/randomPokemons.ts
@@ -1,38 +1,38 @@
 import { PokemonData, PokemonStat, PokemonType } from "../interfaces";
 
-const generateRandomIds = (count: number): number[] => {
-  const ids = new Set<number>();
-  while (ids.size < count) {
-    const randomId = Math.floor(Math.random() * 151) + 1;
-    ids.add(randomId);
+const generateRandomId = (): number => {
+  return Math.floor(Math.random() * 1000) + 1
+}
+
+const fetchPokemon = async (): Promise<any> => {
+  const id = generateRandomId();
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    if (!response.ok)
+      return fetchPokemon();
+    return response.json();
+  } catch (error) {
+    return fetchPokemon();
   }
-  return Array.from(ids);
-};
+}
 
 export const fetchRandomPokemon = async (
   count: number,
 ): Promise<PokemonData[]> => {
-  const randomIds = generateRandomIds(count);
-
-  const fetchPromises = randomIds.map((id) =>
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => {
-      if (!response.ok) {
-        throw new Error(`Kunde inte hämta Pokémon med ID ${id}`);
-      }
-      return response.json();
-    }),
-  );
-
-  const data = await Promise.all(fetchPromises);
-  const transformedPokemons: PokemonData[] = data.map((pokemon) => ({
-              name: pokemon.name,
-              id: pokemon.id,
-              image: pokemon.sprites.other["official-artwork"].front_default || null,
-              types: pokemon.types.map((typeObj: PokemonType) => typeObj.type.name),
-              hp: pokemon.stats.find((statObj: PokemonStat) => statObj.stat.name === 'hp')?.base_stat || 0,
-              attack: pokemon.stats.find((statObj: PokemonStat) => statObj.stat.name === 'attack')?.base_stat || 0,
-              defense: pokemon.stats.find((statObj: PokemonStat) => statObj.stat.name === 'defense')?.base_stat || 0,
-          }));
-
+  const fetchPromises: Promise<any>[] = [];
+  for (let i = 0; i < count; i++) {
+    fetchPromises.push(fetchPokemon());
+  }
+  const pokemonData = await Promise.all(fetchPromises);
+  const transformedPokemons: PokemonData[] = pokemonData.map((pokemon) => ({
+    name: pokemon.name,
+    id: pokemon.id,
+    image: pokemon.sprites.other["official-artwork"].front_default || null,
+    types: pokemon.types.map((typeObj: PokemonType) => typeObj.type.name),
+    hp: pokemon.stats.find((statObj: PokemonStat) => statObj.stat.name === 'hp')?.base_stat || 0,
+    attack: pokemon.stats.find((statObj: PokemonStat) => statObj.stat.name === 'attack')?.base_stat || 0,
+    defense: pokemon.stats.find((statObj: PokemonStat) => statObj.stat.name === 'defense')?.base_stat || 0,
+  }));
+  transformedPokemons.sort((a, b) => a.id - b.id);
   return transformedPokemons
 };
