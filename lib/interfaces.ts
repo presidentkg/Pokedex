d6a5d50e2cc17f276cc354fc09b7e88bd91ad410
Karl Ragnar <karlragnar.kling@gmail.com>
2025-08-28 12:51:18 +0200

    Created a few new interfaces to handle the new fetch function

--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -1,3 +1,8 @@
+export interface Pokemon{
+  name: string;
+  url: string;
+}
+
 export interface PokemonData {
   name: string;
   id: number;
@@ -6,4 +11,19 @@ export interface PokemonData {
   hp: number;
   attack: number;
   defense: number;
+}
+
+export interface PokemonType {
+  slot: number;
+  type: {
+      name: string;
+      url: string;
+  };
+}
+
+export interface PokemonStat {
+  base_stat: number;
+  stat: {
+    name: string;
+  };
 }
