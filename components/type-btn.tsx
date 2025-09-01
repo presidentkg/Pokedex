b8198ad7ce8aad61f8afdcee02e0a62317b9453a
Karl Ragnar <karlragnar.kling@gmail.com>
2025-09-01 14:04:27 +0200

    Added styling on button depending on state and made 2nd click remove the showing pokemon cards

--- components/type-btn.tsx
+++ components/type-btn.tsx
@@ -1,22 +1,25 @@
 "use client";
 
-import { typeColorsBg } from "@/lib/pokemon-colors";
+import { textColors, typeColorsBg, typeColorsBgLighter, typeColorsBorder } from "@/lib/pokemon-colors";
 
 export default function TypeBtn({
   type,
   onClick,
+  isActive,
 }: {
   type: string;
   onClick: (type: string) => void;
+  isActive: boolean;
 }) {
-    return(
+  const activeClassName = isActive ? `border-4 ${typeColorsBorder[type]} ${typeColorsBgLighter[type]} ${textColors[type]}` : `text-white ${typeColorsBg[type]}`;
+  return(
     <div>
         <button
-            className={`p-2 pl-6 pr-6 mt-4 mb-14 cursor-pointer text-xl text-white rounded-full font-bold flex items-center justify-center gap-1 ${typeColorsBg[type]}`} 
+            className={`p-2 pl-6 pr-6 mt-4 mb-14 cursor-pointer text-xl rounded-full font-bold flex items-center justify-center gap-1 ${activeClassName}`} //${typeColorsBg[type]}
             onClick={() => onClick(type)}
         >
             {type}
         </button>
     </div>
-    );
+  );
 }
--- components/types-display.tsx
+++ components/types-display.tsx
@@ -8,45 +8,61 @@ import { PokemonData } from "@/lib/interfaces";
 
 export default function TypeDisplays() {
     const [pokemonList, setPokemon] = useState<PokemonData[] | null>(null);
+    const [activeType, setActiveType] = useState<string | null>(null);
 
     const handlePokemonBtnClick = async (Type: string) => {
-        if (pokemonList) {
+        if(Type === activeType){
             setPokemon(null);
+            setActiveType(null);
         }
-        try{
-            const data = await fetchPokemonType(Type);
-            if(data.length !== 0)
-                setPokemon(data);
-        } catch (error) {
-            setPokemon(null);
-            return <div>Couldn't load pokemon, try again later!</div>;
+        else{
+            setActiveType(Type);
+            try{
+                const data = await fetchPokemonType(Type);
+                if(data.length !== 0)
+                    setPokemon(data);
+            } catch (error) {
+                setPokemon(null);
+                return <div>Couldn't load pokemon, try again later!</div>;
+            }
         }
+        // if (pokemonList) {
+        //     setPokemon(null);
+        // }
+        // try{
+        //     const data = await fetchPokemonType(Type);
+        //     if(data.length !== 0)
+        //         setPokemon(data);
+        // } catch (error) {
+        //     setPokemon(null);
+        //     return <div>Couldn't load pokemon, try again later!</div>;
+        // }
     };
 
     return (
         <div>
             <section className="flex flex-col items-center gap-4">
                 <div className="flex flex-wrap gap-4 justify-center">
-                    <TypeBtn type="normal" onClick={handlePokemonBtnClick}/>
-                    <TypeBtn type="fire" onClick={handlePokemonBtnClick}/>
-                    <TypeBtn type="water" onClick={handlePokemonBtnClick}/>
-                    <TypeBtn type="electric" onClick={handlePokemonBtnClick}/>
-                    <TypeBtn type="grass" onClick={handlePokemonBtnClick}/>
-                    <TypeBtn type="ice" onClick={handlePokemonBtnClick}/>
-                    <TypeBtn type="fighting" onClick={handlePokemonBtnClick}/>
-                    <TypeBtn type="poison" onClick={handlePokemonBtnClick}/>
-                    <TypeBtn type="ground" onClick={handlePokemonBtnClick}/>
+                    <TypeBtn type="normal" onClick={handlePokemonBtnClick} isActive={activeType === "normal"}/>
+                    <TypeBtn type="fire" onClick={handlePokemonBtnClick} isActive={activeType === "fire"}/>
+                    <TypeBtn type="water" onClick={handlePokemonBtnClick} isActive={activeType === "water"}/>
+                    <TypeBtn type="electric" onClick={handlePokemonBtnClick} isActive={activeType === "electric"}/>
+                    <TypeBtn type="grass" onClick={handlePokemonBtnClick} isActive={activeType === "grass"}/>
+                    <TypeBtn type="ice" onClick={handlePokemonBtnClick} isActive={activeType === "ice"}/>
+                    <TypeBtn type="fighting" onClick={handlePokemonBtnClick} isActive={activeType === "fighting"}/>
+                    <TypeBtn type="poison" onClick={handlePokemonBtnClick} isActive={activeType === "poison"}/>
+                    <TypeBtn type="ground" onClick={handlePokemonBtnClick} isActive={activeType === "ground"}/>
                 </div>
                 <div className="flex flex-wrap gap-4 justify-center">
-                    <TypeBtn type="flying" onClick={handlePokemonBtnClick}/>
-                    <TypeBtn type="psychic" onClick={handlePokemonBtnClick}/>
-                    <TypeBtn type="bug" onClick={handlePokemonBtnClick}/>
-                    <TypeBtn type="rock" onClick={handlePokemonBtnClick}/>
-                    <TypeBtn type="ghost" onClick={handlePokemonBtnClick}/>
-                    <TypeBtn type="dragon" onClick={handlePokemonBtnClick}/>
-                    <TypeBtn type="steel" onClick={handlePokemonBtnClick}/>
-                    <TypeBtn type="dark" onClick={handlePokemonBtnClick}/>
-                    <TypeBtn type="fairy" onClick={handlePokemonBtnClick}/>
+                    <TypeBtn type="flying" onClick={handlePokemonBtnClick} isActive={activeType === "flying"}/>
+                    <TypeBtn type="psychic" onClick={handlePokemonBtnClick} isActive={activeType === "psychic"}/>
+                    <TypeBtn type="bug" onClick={handlePokemonBtnClick} isActive={activeType === "bug"}/>
+                    <TypeBtn type="rock" onClick={handlePokemonBtnClick} isActive={activeType === "rock"}/>
+                    <TypeBtn type="ghost" onClick={handlePokemonBtnClick} isActive={activeType === "ghost"}/>
+                    <TypeBtn type="dragon" onClick={handlePokemonBtnClick} isActive={activeType === "dragon"}/>
+                    <TypeBtn type="steel" onClick={handlePokemonBtnClick} isActive={activeType === "steel"}/>
+                    <TypeBtn type="dark" onClick={handlePokemonBtnClick} isActive={activeType === "dark"}/>
+                    <TypeBtn type="fairy" onClick={handlePokemonBtnClick} isActive={activeType === "fairy"}/>
                 </div>
             </section>
             <div className="flex flex-wrap gap-8 mt-8 justify-center">
